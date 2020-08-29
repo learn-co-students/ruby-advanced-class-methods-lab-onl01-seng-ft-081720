1167e7e299cd46e982700e40a381179418063427
redebron20 <redebron20@gmail.com>
2020-08-28 21:04:55 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,65 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+  
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    @@all << new_song
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    @@all << new_song
+    new_song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song then
+      return song
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
+    new_song
+  end
+
+  def self.create_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
+    @@all << new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
